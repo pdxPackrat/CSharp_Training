671f08bf24229f2ba1ae147e730b8c556c994048
Michael Christenson <mdc88@pdxchristenson.com>
2018-05-07 19:29:01 -0700

    A few Lambda-related updates
    The func delegate training (www.tutorialsteacher.com/csharp/csharp-func-delegate) made it very clear.

---
+++ CSharp_tutorials/Tutorial_ImplicitTypeVar/Tutorial_UnitTest/Temperature.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temperature_Example
+{
+    class Temperature
+    {
+        private double _temp;
+
+        public double Temp
+        {
+            get { return _temp;  }
+            set { _temp = value;  }
+        }
+
+        public Temperature(double d) 
+        {
+            Temp = d;
+        }
+    }
+}
--- CSharp_tutorials/Tutorial_ImplicitTypeVar/Tutorial_UnitTest/UnitTest1.cs
+++ CSharp_tutorials/Tutorial_ImplicitTypeVar/Tutorial_UnitTest/UnitTest1.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Temperature_Example;
 
 namespace Tutorial_UnitTest
 {
+
+    public delegate bool TestTemperature(double t);
+
     [TestClass]
     public class UnitTest1
     {
@@ -37,5 +41,31 @@ namespace Tutorial_UnitTest
 
             Assert.AreEqual(true, b.GetType().ToString().Equals("System.Boolean"));
         }
+
+        [TestMethod]
+        public void TestLambdaExpressions()
+        {
+#pragma warning disable IDE0039 // Use local function
+            Func<Temperature, bool> isTemperatureModerate = t => t.Temp > 50.00 && t.Temp < 90.00;
+#pragma warning restore IDE0039 // Use local function
+
+            Temperature myTemp = new Temperature(80.05d);
+
+            bool myResult = isTemperatureModerate(myTemp); // should return true
+
+            Assert.AreEqual(true, myResult);
+        }
+
+        [TestMethod]
+        public void TestLambdaExpressions2()
+        {
+#pragma warning disable IDE0039 // Use local function
+            Func<int, int, int> Sum = (x, y) => x + y;
+#pragma warning restore IDE0039 // Use local function
+            int myResult = Sum(10,20);
+
+            Assert.AreEqual(30, myResult);
+        }
+
     }
 }
